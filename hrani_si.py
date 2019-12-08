578b1077d70d88703b69ddbb5f01ba3d59171ff0
Kubiczek36 <kubadokulil99@gmail.com>
2019-12-08 18:25:00 +0100

    fljg

--- hrani_si.py
+++ hrani_si.py
@@ -3,31 +3,46 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from scipy import ndimage
 
-fileName = 'imgs/2.tiff'
+fileName1 = 'imgs/6.tiff'
+fileName2 = 'imgs/-2.tiff'
 
-def imgRead(name, trashold):
+def imgRead(name, trashold = 30):
     im = Image.open(name).convert('LA')
     imarray = np.array(im)[:,:,0]
     trasholdImArray = (imarray > trashold) * imarray
     return trasholdImArray
 
-def getPSF(ImArray, size):
+def getPSF(ImArray, size = 50):
     cg = ndimage.measurements.center_of_mass(ImArray) ## najde těžiště
     cg = np.array([cg[0], cg[1]]).astype(int)
-    ImArray[cg[0], cg[1]] = 255 ### dávat?
+#    ImArray[cg[0], cg[1]] = 255 ### dávat?
     PSF = ImArray[(cg[0]-(size//2)):(cg[0]+(size//2)), (cg[1]-(size//2)):(cg[1]+(size//2))]
     plt.imshow(PSF)
-    return PSF, cg
+    return PSF
 
-def getBinaryPSF(ImArray, size, trashold):
+def getBinaryPSF(ImArray, size = 50, trashold = 30):
     cg = ndimage.measurements.center_of_mass(ImArray) ## najde těžiště
     cg = np.array([cg[0], cg[1]]).astype(int)
-    ImArray[cg[0], cg[1]] = 255 ### dávat?
+#    ImArray[cg[0], cg[1]] = 255 ### dávat?
     PSF = ImArray[(cg[0]-(size//2)):(cg[0]+(size//2)), (cg[1]-(size//2)):(cg[1]+(size//2))]
     PSF = (PSF > trashold) * 1
-    plt.imshow(PSF)
+    #plt.imshow(PSF)
     return PSF, cg
 
 
-trshld = 30 #trešhold vole!
-PSFsize = 40//2
+array1 = imgRead(fileName1)
+#arr1 = plt.imshow(array1)
+PSF1, cg1 = getBinaryPSF(array1)
+#PSF1im = plt.imshow(PSF1)
+print(fileName1)
+print("souradnice1:", cg1)
+
+array2 = imgRead(fileName2)
+#plt.imshow(array2)
+PSF2, cg2 = getBinaryPSF(array2)
+#plt.imshow(PSF2)
+print(fileName2)
+print("--------------- \n souradnice2:", cg2)
+
+plt.imshow(PSF1 - PSF2)
+print(np.sum(abs(PSF1 - PSF2)))
