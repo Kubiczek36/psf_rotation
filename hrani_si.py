9a1453be1a8abd111212ad4df138a11436fadb67
Jakub Dokulil <kuba@ufi-18.fme.vutbr.cz>
2020-01-16 15:25:30 +0100

    rozpracovaná funkce pro evaluaci, pokus zakomponovat while cyklus

--- hrani_si.py
+++ hrani_si.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from scipy import ndimage
 
-fileName1 = 'imgs/6.tiff'
-fileName2 = 'imgs/-2.tiff'
+fileName0 = 'imgs/0.tiff'
+fileName1 = 'imgs/2.tiff'
+fileName_1 = 'imgs/-2.tiff'
+fileName2 = 'imgs/4.tiff'
 
 def imgRead(name, trashold = 30):
     im = Image.open(name).convert('LA')
@@ -29,6 +31,51 @@ def getBinaryPSF(ImArray, size = 50, trashold = 30):
     #plt.imshow(PSF)
     return PSF, cg
 
+def condition(difference, refPlus, refMinus): ### Podmínka posunu
+    return (max(refPlus, refMinus)/2 < difference)
+
+class PSF:
+    def __init__(self, path, trashold = 30):
+        self.path = path
+        self.img = Image.open(self.path).convert('LA')
+        imarray = np.array(self.img)[:,:,0]
+        self.tArray = (imarray > trashold) * imarray
+        self.PSFArray, self.cg = getBinaryPSF(self.tArray)
+    def compare(self, second):
+        plt.imshow(self.PSFArray - second.PSFArray)
+        return np.sum(abs(self.PSFArray - second.PSFArray))
+       
+
+refer = PSF(fileName0)
+referPlus = PSF(fileName1)
+referMinus = PSF(fileName_1)
+
+# stage.move(+1)
+stepPlus = refer.compare(referPlus)
+# stage.move(-2)
+stepMinus = refer.compare(referMinus)
+# stage.move(+1)
+
+direction = 1
+
+def evaluation(ref, plus, minus):
+    dif = []
+    # current = PSF(camera.getImg())
+    current = PSF(fileName2)
+    dif.append(ref.compare(current))
+    while condition(dif[len(dif)], plus, minus):
+        movenment = dif/np.mean(plus, minus)
+        print(movenment)
+        # stage.move(movenment)
+        # current = PSF(camera.getImg())
+        dif.append(ref.compare(current))
+        
+
+"""
+zkusit dorovnat na přesno, ale když to bude dlouho pendlovat a bude to jakž takž v mezi tak to nechat
+"""
+    else:
+        print('Asi OK. rozdíl obrázků dif = ', dif)
 
 array1 = imgRead(fileName1)
 #arr1 = plt.imshow(array1)
@@ -37,11 +84,11 @@ PSF1, cg1 = getBinaryPSF(array1)
 print(fileName1)
 print("souradnice1:", cg1)
 
-array2 = imgRead(fileName2)
+array2 = imgRead(fileName0)
 #plt.imshow(array2)
 PSF2, cg2 = getBinaryPSF(array2)
 #plt.imshow(PSF2)
-print(fileName2)
+print(fileName0)
 print("--------------- \n souradnice2:", cg2)
 
 plt.imshow(PSF1 - PSF2)
